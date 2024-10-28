7863381483222da50d3ccb1e559ba166d6624f02
Steven-FH <s2410238018@fhooe.at>
2024-10-28 17:40:04 +0100

    UE_02

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -8,9 +8,9 @@ public class ue02_3 {
         double x = In.readDouble();
         Out.println("please enter y-coordinate");
         double y = In.readDouble();
-        if (x < 0 || x > 4 || y < 0 || y > 4) {
+        if (x < 0 || x >= 4 || y < 0 || y >= 4) {
             Out.println("The point is not in the green area");
-        } else if( Math.sqrt(x*x + y*y) <= 4) {
+        } else if(Math.sqrt(x*x + y*y) <= 4) {
             Out.println("The point is not in the green area");
         } else {
             Out.println("The point is in the green area");
